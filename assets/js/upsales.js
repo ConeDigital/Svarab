e7c00874e6d78bd1f926e63ef846a6c19a5d357f
mikaelbang <mackeb1@gmail.com>
2017-02-17 16:07:06 +0100

    removed upsales

--- assets/js/upsales.js
+++ assets/js/upsales.js
@@ -1,28 +1,28 @@
-jQuery(document).ready( function($) {
-    /*
-     * Create Contact in Upsales CRM
-     */
-    $('.offer-form-submit').on('click', function () {
-        event.preventDefault();
+// jQuery(document).ready( function($) {
+//     /*
+//      * Create Contact in Upsales CRM
+//      */
+//     $('.offer-form-submit').on('click', function () {
+//         event.preventDefault();
 
-        $.ajax({
-            url: ajaxApi.ajaxurl,
-            type: 'POST',
-            data: {
-                action: 'api_upsales_ajax',
-                name: $('.offer-name').val(),
-                email: $('.offer-email').val(),
-                phone: $('.offer-phone').val(),
-                notes: $('.offer-notes').val(),
-            },
-            success: function(data) {
-                console.log('Success');
-                console.log(data);
-            },
-            error: function(response) {
-                console.log(response.response);
-                console.log('Error!!!');
-            }
-        });
-    });
-}); 
+//         $.ajax({
+//             url: ajaxApi.ajaxurl,
+//             type: 'POST',
+//             data: {
+//                 action: 'api_upsales_ajax',
+//                 name: $('.offer-name').val(),
+//                 email: $('.offer-email').val(),
+//                 phone: $('.offer-phone').val(),
+//                 notes: $('.offer-notes').val(),
+//             },
+//             success: function(data) {
+//                 console.log('Success');
+//                 console.log(data);
+//             },
+//             error: function(response) {
+//                 console.log(response.response);
+//                 console.log('Error!!!');
+//             }
+//         });
+//     });
+// }); 
