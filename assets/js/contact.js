3510b72603bfb1de92e54a9be80015e4922ceba8
mikaelbang <mackeb1@gmail.com>
2017-02-20 16:29:23 +0100

    remove modal

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,5 +9,22 @@ jQuery(document).ready( function($) {
         $('.contact-retailer-modal').show();
     });
 
+    $('.close-c-modal').on('click', function(){
+    	$('.contact-retailer-modal').hide();
+    });
+
+    $(document).on('click keyup', function(event) {
+    	
+    	//Get DOM-element of container
+    	var element = $('.contact-retailer-modal')[0];
+    	//If press esc, close modal
+    	if (event.keyCode === 27) $('.close-c-modal').click();
+
+    	//If clicked object isnt <a> and clicked element isnt in container
+    	if( event.target.tagName != 'A' && ! $.contains(element, event.toElement) ){
+    		//Hide container
+    		$('.contact-retailer-modal').hide();
+    	} 
+	});
 
 });
